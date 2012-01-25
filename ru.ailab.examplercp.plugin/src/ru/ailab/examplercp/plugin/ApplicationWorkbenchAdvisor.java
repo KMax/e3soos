ddde58029341e51f675eb7b9cce9e5eac8bd0bc2
Maxim Kolchin <kolchinmax@gmail.com>
2012-01-25 15:44:27 +0400

    A Eclipse RCP skeleton application has been created.

---
+++ ru.ailab.examplercp.plugin/src/ru/ailab/examplercp/plugin/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,18 @@
+package ru.ailab.examplercp.plugin;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "ru.ailab.examplercp.plugin.perspective"; //$NON-NLS-1$
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}

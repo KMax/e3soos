0a11bb15bd1bec054011e0c131e4487f427aecd8
Maxim Kolchin <kolchinmax@gmail.com>
2012-01-28 16:56:00 +0400

    Packages were renamed. The parent pom.xml was moved to the ru.ailab.e3soos directory.

---
+++ ru.ailab.e3soos.plugin/src/ru/ailab/e3soos/plugin/Perspective.java
@@ -0,0 +1,10 @@
+package ru.ailab.e3soos.plugin;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}

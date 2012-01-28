9ab4ce63f0e3342321c7b1086b931aa8d317fd79
Maxim Kolchin <kolchinmax@gmail.com>
2012-01-29 02:40:44 +0400

    Target Definition File was added

--- ru.ailab.e3soos.plugin/src/ru/ailab/e3soos/plugin/ApplicationWorkbenchAdvisor.java
+++ ru.ailab.e3soos.plugin/src/ru/ailab/e3soos/plugin/ApplicationWorkbenchAdvisor.java
@@ -6,9 +6,10 @@ import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
-	private static final String PERSPECTIVE_ID = "ru.ailab.e3soos.plugin.perspective"; //$NON-NLS-1$
+	private static final String PERSPECTIVE_ID = "ru.ailab.e3soos.plugin.perspective";
 
-    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+    		IWorkbenchWindowConfigurer configurer) {
         return new ApplicationWorkbenchWindowAdvisor(configurer);
     }
 

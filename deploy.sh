60f74ea436615fa8b1ddbb07e55e7e3c0fdfbe07
Maxim Kolchin <kolchinmax@gmail.com>
2012-05-08 14:15:36 +0400

    The deployment script was rewritten.

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,59 @@
 #!/bin/bash
+# deploy.sh
 
-echo "[INFO] [deploy.sh] Building a war..."
-cd web
-play war --output=../e3soos.war --exclude=nbproject:test-result:tmp --%production
-cd ..
-echo "[INFO] [deploy.sh] The war has been succesfully built."
+HOST=192.168.134.7
+USER=guvnor
+WAR_NAME=e3soos.war
+DESTINATION=/opt/jboss-as/standalone/deployments/$WAR_NAME/
 
-echo "[INFO] [deploy.sh] Undeploying the previous version..."
-ssh guvnor@192.168.134.7 'cd /opt/jboss-as/standalone/deployments; mv -f e3soos.war.deployed e3soos.war.undeploy; sleep 2s; rm -f e3soos.war.undeploy*; cd e3soos.war/WEB-INF; rm -r application classes framework lib resources;'
-echo "[INFO] [deploy.sh] The previous version has been undeployed."
+build()
+{
+	echo "[INFO] [deploy.sh] Building a war file..."
+	pushd web
+	play war --output=../$WAR_NAME --exclude=nbproject:test-result:tmp --%production
+	popd
+	echo "[INFO] [deploy.sh] The war was built successfully."
+}
 
-echo "[INFO] [deploy.sh] Copying the sources..."
-scp -rC e3soos.war/* guvnor@192.168.134.7:/opt/jboss-as/standalone/deployments/e3soos.war/
-echo "[INFO] [deploy.sh] The sources has been copied."
+deploy()
+{
+	echo "[INFO] [deploy.sh] Deploying the war..."
+	echo "" >> $WAR_NAME.dodeploy
+	scp -C $WAR_NAME.dodeploy $USER@$HOST:/opt/jboss-as/standalone/deployments/$WAR_NAME.dodeploy $1 > /dev/null
+	rm -f $WAR_NAME.dodeploy
+	echo "[INFO] [deploy.sh] The war was deployed and the app was started."
+}
 
-echo "[INFO] [deploy.sh] Deploying..."
-scp -C deploy.sh guvnor@192.168.134.7:/opt/jboss-as/standalone/deployments/e3soos.war.dodeploy $1 > /dev/null
-echo "[INFO] [deploy.sh] The war has been deployed."
+undeploy()
+{
+	echo "[INFO] [deploy.sh] Undeploying the previously deployed war..."
+        ssh $USER@$HOST 'cd /opt/jboss-as/standalone/deployments; rm -f $WAR_NAME.*; touch $WAR_NAME.undeploy; sleep 2s; rm -f $WAR_NAME.undeploy*; cd $WAR_NAME/WEB-INF; rm -fr application classes framework lib resources;'
+        echo "[INFO] [deploy.sh] The previous version was undeployed."
+}
+
+copy()
+{
+	echo "[INFO] [deploy.sh] Copying the sources..."
+        scp -rC $WAR_NAME/* $USER@$HOST:$DESTINATION
+        echo "[INFO] [deploy.sh] The sources has been copied."
+}
+
+if [ "$1" = "--deployonly" ]
+then
+	deploy;
+	exit $?;
+fi
+
+if [ "$1" = "--buildonly" ]
+then
+	build;
+	exit $?;
+fi
+
+if [ "$1" = "" ]
+then
+	build;
+	undeploy;
+	copy;
+	deploy;
+fi

29c3bedbdfbacd61449633ed6c1298272ee2d3ef
Maxim Kolchin <kolchinmax@gmail.com>
2012-06-24 15:23:05 +0400

    The #1150 #1152 #1151 fixed.

--- web/public/javascripts/drawing/shapes/Scheme.js
+++ web/public/javascripts/drawing/shapes/Scheme.js
@@ -8,11 +8,48 @@ Drawing.Scheme = function(options) {
     this.options = {};
     $.extend(this.options, this._defaults, options);
 
-    this._calculateOptions({});
-
     this.layer = new Kinetic.Layer();
     this.elements = [];
     this.surfaces = [];
+    this.types = [];
+    this.zones = [];
+    this.inFirstZone = 0;
+    this.inSecondZone = 0;
+    this.inThirdZone = 0;
+
+    for(var i = 0; i < this.options.codes.length; i++) {
+        this.types.push(this.options.codes[i].charAt(2));
+        this.zones.push(parseInt(this.options.codes[i].charAt(1)));
+        this.types.push(this.options.codes[i].charAt(4));
+        this.zones.push(parseInt(this.options.codes[i].charAt(3)));
+        if(this.zones[i*2] == 1 && this.zones[i*2 + 1] == 1) {
+            this.inFirstZone++;
+        }
+        if(this.zones[i*2] == 2 && this.zones[i*2 + 1] == 2) {
+            this.inSecondZone++;
+        }
+        if(this.zones[i*2] == 3 && this.zones[i*2 + 1] == 3) {
+            this.inThirdZone++;
+        }
+    }
+
+    this.options.firstZoneW = this.options.elementWidth * (1 + this.inFirstZone)
+        + this.options.interval * (1 + this.inFirstZone);
+    this.options.firstZSX = this.options.interval;
+    this.options.secondZoneW = this.options.elementWidth * (3 + this.inSecondZone)
+        + this.options.interval * (3 + this.inSecondZone);
+    this.options.secondZSX = this.options.firstZoneW + this.options.interval;
+    this.options.secondZEX = this.options.secondZSX + this.options.secondZoneW;
+    this.options.thirdZoneW = this.options.elementWidth * (1 + this.inThirdZone)
+        + this.options.interval * (1 + this.inThirdZone);
+    this.options.thirdZSX = this.options.secondZEX + this.options.interval,
+
+    this.options.width = this.options.firstZoneW + this.options.interval * 2 + this.options.secondZoneW
+        + this.options.thirdZoneW;
+    this.options.centerX = this.options.secondZSX + this.options.secondZoneW / 2;
+    this.options.centerY = this.options.height / 2;
+    this.options.endX = this.options.width - this.options.xylinesStrokeWidth;
+    this.options.zoneBottomY = (this.options.height - this.options.xylinesStrokeWidth * 2) * 0.95;
 };
 
 Drawing.Scheme.prototype._defaults = {
@@ -25,22 +62,6 @@ Drawing.Scheme.prototype._defaults = {
     xylinesStroke: "black",
     elementWidth: 30,
     elementHeight: 100
-};
-
-Drawing.Scheme.prototype._calculateOptions = function (options) {
-    $.extend(this.options, options);
-
-    //Calculate the options
-    this.options.centerX = this.options.width / 2;
-    this.options.centerY = this.options.height / 2;
-    this.options.endX = this.options.width - this.options.xylinesStrokeWidth;
-    this.options.secondZoneWidth = this.options.elementWidth * 3 + this.options.interval * 3;
-    this.options.secondZSX = this.options.centerX - (this.options.secondZoneWidth / 2);
-    this.options.secondZEX = this.options.centerX + (this.options.secondZoneWidth / 2);
-    this.options.firstZSX = this.options.interval,
-    this.options.thirdZSX = this.options.secondZEX,
-    this.options.thirdZEX = this.options.width - this.options.interval;
-    this.options.zoneBottomY = (this.options.height - this.options.xylinesStrokeWidth * 2) * 0.95;
 };
 
 Drawing.Scheme.prototype._createXYLines = function () {
@@ -117,7 +138,6 @@ Drawing.Scheme.prototype._createXYLines = function () {
 };
 
 Drawing.Scheme.prototype._addElement = function (element) {
-    //The element's props
     var y = (this.options.height - element.getHeight()) / 2,
         x = this.options.firstZSX,
         w = this.options.elementWidth;
@@ -131,27 +151,17 @@ Drawing.Scheme.prototype._addElement = function (element) {
         if(previous != undefined) {
             x = previous.getX() + previous.getWidth() + this.options.interval;
         }
-        var temp = this.options.secondZSX - this.options.elementWidth / 2;
-        if(element.getSecondZone() == "1") {
-            var elRightX = x + w + this.options.interval;
-            if(elRightX > temp) {
-                this._calculateOptions({
-                    width: this.options.width + (elRightX - temp) * 2
-                });
-            }
-        } else if(element.getSecondZone() == "2") {
-            x = temp;
+        if(element.getSecondZone() == "2") {
+            x = this.options.secondZSX - this.options.elementWidth / 2;
         } else if(element.getSecondZone() == "3") {
-            x = temp;
-            w = this.options.thirdZSX + (this.options.elementWidth / 2) - x;
+            x = this.options.secondZSX - this.options.elementWidth / 2;
+            w = this.options.secondZEX + (this.options.elementWidth / 2) - x;
         }
     } else if(element.getFirstZone() == "2") {
-        x = this.options.secondZSX + this.options.interval;
-        if(element.getSecondZone() == "2") {
-            if(previous != undefined && previous.getFirstZone() == "2" && previous.getSecondZone() == "2") {
-                x = previous.getX() + previous.getWidth();
-            } else {
-                x = this.options.centerX + this.options.interval;
+        x = this.options.centerX + this.options.interval;
+        if(element.getSecondZone() == "2" && previous != undefined) {
+            if(previous.getFirstZone() == "2" && previous.getSecondZone() == "2") {
+                x = previous.getX() + previous.getWidth() + this.options.interval;
             }
         } else if(element.getSecondZone() == "3") {
             x = this.options.secondZEX - this.options.elementWidth / 2;
@@ -177,28 +187,19 @@ Drawing.Scheme.prototype._addElement = function (element) {
 /**
  * @return one of [-1, 0, 1] numbers
  */
-Drawing.Scheme.prototype._getSaliences = function (codes) {
+Drawing.Scheme.prototype._getSaliences = function () {
     var results = [];
-    var types = [];
-    var zones = [];
-
-    for(var i = 0; i < codes.length; i++) {
-        types.push(codes[i].charAt(2));
-        zones.push(parseInt(codes[i].charAt(1)));
-        types.push(codes[i].charAt(4));
-        zones.push(parseInt(codes[i].charAt(3)));
-    }
 
     //console.log(types);
     //console.log(zones);
 
     var b = 0;
 
-    for(var i = 0; i < types.length; i++) {
-        if(types[i] == "P") {
-            if(i % 2 != 0 && zones[i - 1] == 1 && zones[i] < 3) {
+    for(var i = 0; i < this.types.length; i++) {
+        if(this.types[i] == "P") {
+            if(i % 2 != 0 && this.zones[i - 1] == 1 && this.zones[i] < 3) {
                 results[i] = 1;
-            } else if(i % 2 == 0 && zones[i] < 2){
+            } else if(i % 2 == 0 && this.zones[i] < 2){
                 results[i] = 1;
             } else {
                 results[i] = -1;
@@ -208,9 +209,9 @@ Drawing.Scheme.prototype._getSaliences = function (codes) {
             } else {
                 b = -1 * results[i];
             }
-        } else if(types[i] == "O") {
+        } else if(this.types[i] == "O") {
             results[i] = 0;
-        } else if(types[i] == "A") {
+        } else if(this.types[i] == "A") {
             if(i == 0) {
                 results[i] = 0;
             } else {
@@ -220,12 +221,14 @@ Drawing.Scheme.prototype._getSaliences = function (codes) {
                     results[i] = -1;
                 }
             }
-        } else if(types[i] == "F") {
+        } else if(this.types[i] == "F") {
             if(b == 1) {
                 results[i] = 1;
             } else {
                 results[i] = -1;
             }
+        } else if(this.types[i] == "V" && this.zones[i] == 2){
+            results[i] = -1; //FIXME
         } else {
             results[i] = 0;
         }
--- web/public/javascripts/drawing/tests.js
+++ web/public/javascripts/drawing/tests.js
@@ -1,4 +1,10 @@
 
-var schemes = ['Y1A1P + B1P1A + T3A3F', 'Y1A1P + Y1F1V + B1P1A + T1A2F + T2P2P + T2F2F + T3A3F'];
+var schemes = [
+    'B1A3A',
+    'Y1A1A + B2A2P + T3A3A',
+    'Y2V2P + B2A2P + T3V3P',
+    'Y1A2A + B2A3F',
+    'Y1A1P + Y1F1V + B1P1A + C1A2F + T2P2P + T2F2F + T2A3A + T3A3F + T3V3V + T3A3A'
+];
 
 lib.schemes.show({schemes: schemes});
